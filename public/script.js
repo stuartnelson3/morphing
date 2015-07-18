e0696f3e1d431b995e9168c1354bd8fd33285374
Stuart Nelson <stuartnelson3@gmail.com>
2015-07-18 19:28:19 +0200

    extract sphere coordinate function

--- public/script.js
+++ public/script.js
@@ -9,14 +9,14 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // create the particle variables
-var particleCount = 50,
-    particles = new THREE.Geometry(),
-    pMaterial = new THREE.ParticleBasicMaterial({
-      color: 0xFFFFFF,
-      size: 1,
-      blending: THREE.AdditiveBlending,
-      transparent: true
-    });
+var PARTICLE_COUNT = 50;
+var particles = new THREE.Geometry();
+var pMaterial = new THREE.ParticleBasicMaterial({
+  color: 0xFFFFFF,
+  size: 1,
+  blending: THREE.AdditiveBlending,
+  transparent: true
+});
 
 scene.add(camera);
 
@@ -28,31 +28,38 @@ cos = Math.cos;
 sin = Math.sin;
 tan = Math.tan;
 
-// now create the individual particles
-function generateParticles(n) {
-  var lim = Math.PI;
-  var a = [];
-  var inc = lim / n;
-  var i = 0;
-  for (var u = 0; u < Math.PI; u+=inc) {
-    for (var v = 0; v < Math.PI; v+=inc) {
-
-      // create a particle with random
-      // position values, -250 -> 250
-      var pX = sin(v)*cos(u),
-          pY = cos(pi2)*sin(v)*sin(u) - sin(pi2)*cos(v),
-          pZ = sin(pi2)*sin(v)*sin(u) + cos(pi2)*cos(v),
-          particle = new THREE.Vertex(
-            new THREE.Vector3(SPACING*pX, SPACING*pY, SPACING*pZ)
-          );
-
-      // add it to the geometry
-      a[i] = particle;
-      i++;
-    }
+var sphereParticle = {
+  uLim: Math.PI/12,
+  vLim: Math.PI,
+  fn: function(u, v) {
+    var pX = sin(v)*cos(u);
+    var pY = cos(pi2)*sin(v)*sin(u) - sin(pi2)*cos(v);
+    var pZ = sin(pi2)*sin(v)*sin(u) + cos(pi2)*cos(v);
+    return  new THREE.Vertex(
+      new THREE.Vector3(SPACING*pX, SPACING*pY, SPACING*pZ)
+    );
   }
-  return a;
+};
+
+// now create the individual particles
+function generateParticles(n, particleObj) {
+  var p = particleObj;
+  var a = [];
+  var inc = Math.PI / n;
+  var i = 0;
+
+  return function() {
+    for (var u = 0; u < p.uLim; u+=inc) {
+      for (var v = 0; v < p.vLim; v+=inc) {
+        a[i] = p.fn(u,v);
+        i++;
+      }
+    }
+    return a;
+  };
 }
+
+var sphere = generateParticles(PARTICLE_COUNT, sphereParticle);
 
 // create the particle system
 var particleSystem = new THREE.ParticleSystem(
@@ -63,7 +70,7 @@ particleSystem.sortParticles = true;
 // add it to the scene
 scene.add(particleSystem);
 
-particles.vertices = generateParticles(particleCount);
+particles.vertices = sphere();
 particleSystem.rotation.y = pi2;
 function update() {
   particleSystem.rotation.y += 0.001;
