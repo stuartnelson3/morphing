d259b8cd2babd4a04dffd639d8cca6583479d2c2
Stuart Nelson <stuartnelson3@gmail.com>
2015-07-19 18:59:58 +0200

    mousetrap, gui menu, 2d mesh display

--- public/script.js
+++ public/script.js
@@ -9,10 +9,11 @@ tan = Math.tan;
 COLOR = 0xFFA500;
 SPACING = 100;
 CAMERA_DISTANCE = SPACING*5;
+DRAW_DISTANCE = 1500;
 PARTICLE_COUNT = 50;
 
 var sphereParticle = {
-  uLim: Math.PI,
+  uLim: 2*Math.PI,
   vLim: Math.PI,
   fn: function(u, v) {
     var pX = sin(v)*cos(u);
@@ -81,29 +82,43 @@ var swirl = generateParticles(PARTICLE_COUNT, swirlParticle);
 var twoSphere = generateParticles(PARTICLE_COUNT, twoSphereParticle);
 var threeSphere = generateParticles(PARTICLE_COUNT, threeSphereParticle);
 
+var shapes = {
+  sphere: sphere,
+  swirl: swirl,
+  twoSphere: twoSphere,
+  threeSphere: threeSphere
+}
+
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1500);
+var camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, DRAW_DISTANCE);
 scene = new THREE.Scene();
 
 var material = new THREE.LineBasicMaterial({
   color: COLOR
 });
 
-// s = swirl();
-// s = twoSphere();
-s = threeSphere();
-// s = sphere();
+function createScene(scene, shape) {
+  // latitudinal curves
+  for (var i = 0; i < PARTICLE_COUNT; i++) {
+    var geometry = new THREE.Geometry();
 
-for (var i = 0; i < PARTICLE_COUNT; i++) {
-  var ringIndex = i;
-  var geometry = new THREE.Geometry();
-  for (var j = 0; j < s.length; j++) {
-    geometry.vertices[j] = s[j][i];
+    for (var j = 0; j < shape.length; j++) {
+      geometry.vertices[j] = shape[j][i];
+    }
+    var line = new THREE.Line(geometry, material);
+    scene.add(line);
   }
-  var line = new THREE.Line(geometry, material);
-  scene.add(line);
+
+  // longitudinal curves
+  // TODO: merge these into one nested loop
+  shape.forEach(function(s) {
+    var geometry = new THREE.Geometry();
+    geometry.vertices = s;
+    var line = new THREE.Line(geometry, material);
+    scene.add(line);
+  });
 }
 
 function cameraOrbit(camera) {
@@ -113,11 +128,95 @@ function cameraOrbit(camera) {
   camera.position.z = sin( timer ) * CAMERA_DISTANCE;
 }
 
+camera.position.x = cos(0) * CAMERA_DISTANCE;
+camera.position.z = sin(0) * CAMERA_DISTANCE;
 function update() {
-  cameraOrbit(camera);
   camera.lookAt(scene.position);
   renderer.render(scene, camera);
   requestAnimationFrame(update);
 }
 
 update();
+
+var gui = new dat.GUI({
+    height : 5 * 32 - 1
+});
+
+function removeChildren(scene, cb) {
+  setTimeout(function() {
+    scene.remove(scene.children[0]);
+    if (!scene.children.length) {
+      return (cb||function(){})();
+    }
+
+    removeChildren(scene, cb);
+  }, 20);
+}
+
+var params = {
+  clear: function() {
+    removeChildren(scene);
+  },
+  shape: 'sphere'
+};
+
+createScene(scene, shapes['sphere']());
+
+var controller = gui.add(params, 'shape', Object.keys(shapes) );
+controller.onFinishChange(function(value) {
+  removeChildren(scene, function() {
+    createScene(scene, shapes[value]());
+  });
+});
+
+gui.add(params, 'clear').name('Clear');
+
+(function() {
+
+  rotation = 0;
+  yRotation = 0;
+  // zoom
+  Mousetrap.bind('=', function() {
+    if (CAMERA_DISTANCE < (DRAW_DISTANCE - 2*SPACING)) {
+      CAMERA_DISTANCE += 10;
+    }
+    camera.position.x = cos( rotation ) * CAMERA_DISTANCE;
+    camera.position.z = sin( rotation ) * CAMERA_DISTANCE;
+  });
+
+  Mousetrap.bind('-', function() {
+    if (CAMERA_DISTANCE > 10) {
+      CAMERA_DISTANCE -= 10;
+    }
+    camera.position.x = cos( rotation ) * CAMERA_DISTANCE;
+    camera.position.z = sin( rotation ) * CAMERA_DISTANCE;
+  });
+
+  // rotation
+  Mousetrap.bind('down', function() {
+    rotation += 0.05
+    yRotation += 0.05
+    camera.position.x = cos( rotation ) * CAMERA_DISTANCE;
+    camera.position.y = sin( yRotation ) * CAMERA_DISTANCE;
+  });
+
+  Mousetrap.bind('up', function() {
+    rotation -= 0.05
+    yRotation -= 0.05
+    camera.position.x = cos( rotation ) * CAMERA_DISTANCE;
+    camera.position.y = sin( yRotation ) * CAMERA_DISTANCE;
+  });
+
+  Mousetrap.bind('left', function() {
+    rotation -= 0.05
+    camera.position.x = cos( rotation ) * CAMERA_DISTANCE;
+    camera.position.z = sin( rotation ) * CAMERA_DISTANCE;
+  });
+
+  Mousetrap.bind('right', function() {
+    rotation += 0.05
+    camera.position.x = cos( rotation ) * CAMERA_DISTANCE;
+    camera.position.z = sin( rotation ) * CAMERA_DISTANCE;
+  });
+
+})();
